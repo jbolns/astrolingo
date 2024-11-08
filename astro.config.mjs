42ec6e410f684fcc6fae3f942c215a4d82bdcb93
J <80164315+jbolns@users.noreply.github.com>
2024-11-08 19:30:06 +0200

    GH Pages > Adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "http://localhost:4321/",
+	site: "https://astrolingo.josebolanos.xyz/",
 
 	integrations: [mdx(), sitemap()],
 
